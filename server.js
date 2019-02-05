aaec555644ecd983051fe2ab3bbeafc3d9605801
ssskram <paul.marks@pittsburghpa.gov>
2019-02-05 11:38:07 -0500

    trying to get sockets to work in staging

--- server.js
+++ server.js
@@ -1,52 +1,76 @@
-const express = require('express')
-const cors = require('cors')
-const http = require("http")
-const socketIo = require("socket.io")
-const server = http.createServer(app)
-const fetch = require("node-fetch")
-const io = socketIo(server)
-global.Headers = fetch.Headers
-
-// import env variables
-require('dotenv').config()
-
-// start express
+// Setup basic express server
+var express = require('express')
 var app = express()
-app.set('port', process.env.PORT || 3000)
+var path = require('path')
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+var port = process.env.PORT || 3000
 
-// docs
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-// enable cors on all requests
-app.use(cors())
-
-// logging
-app.use(require('morgan')('combined'))
-
-// socket
-// TODO: routing
-io.on("connection", socket => {
-  //TODO: authorization
-  () => sendData(socket)
-  socket.on("disconnect", () => console.log("Client disconnected"))
+server.listen(port, () => {
+  console.log('Server listening at port %d', port)
 })
-const sendData = async socket => {
-  try {
-    // get endpoint here, return data
-    socket.emit("fromAPI", res.data)
-  } catch (err) {}
-}
 
-// Production error handler
-if (app.get('env') === 'production') {
-  app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.sendStatus(err.status || 500)
+// Routing
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Chatroom
+
+var numUsers = 0
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    })
   })
-}
 
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'))
-})
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    })
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
