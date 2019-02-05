8387262032690f4ef694edcf2b87dd8d5e10e982
ssskram <paul.marks@pittsburghpa.gov>
2019-02-05 12:50:18 -0500

    all dependencies installed, beginning routing refactor now

--- server.js
+++ server.js
@@ -2,25 +2,61 @@
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
-var port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const fetch = require("node-fetch")
+const cors = require('cors')
+global.Headers = fetch.Headers
 
+// import env variables
+require('dotenv').config()
+
+// body parser
+app.use(bodyParser.json())
+
+// enable cors on all requests
+app.use(cors())
+
+// logging
+app.use(require('morgan')('combined'))
+
+// PGH Works
+// activity feed
 var activity = io
   .of('/activity')
   .on('connection', function (socket) {
-    // when client connects, send entirety of activity class, Cartegraph
-    // GET here
-    // return data
-    socket.emit('data', 'activity data coming your way')
+    // when client connects, send entirety of activity class, cartegraph
+    fetch("https://cartegraphapi.azurewebsites.us/pghWorks/activity", {
+        method: 'get',
+        headers: new Headers({
+          'Authorization': 'Bearer ' + process.env.REACT_APP_CART_API
+        })
+      })
+      .then(res => res.json())
+      .then(data => socket.emit('data', data))
 
     // called from client when new data is posted to activity class, cartegraph
     socket.on('update', () => {
-      console.log('update')
-      // GET here
       // broadcast new data to all users here
-      activity.emit('data', 'activity data coming your way')
+      fetch("https://cartegraphapi.azurewebsites.us/pghWorks/activity", {
+          method: 'get',
+          headers: new Headers({
+            'Authorization': 'Bearer ' + process.env.REACT_APP_CART_API
+          })
+        })
+        .then(res => res.json())
+        .then(data => activity.emit('data', data))
     })
   })
 
-server.listen(port, () => {
-  console.log('Server listening at port %d', port)
+// Production error handler
+if (app.get('env') === 'production') {
+  app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.sendStatus(err.status || 500)
+  })
+}
+
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
 })
